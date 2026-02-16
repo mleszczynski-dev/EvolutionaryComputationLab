80b6188e23aaaf7795a26142cdfd06ec5482cd50
Michal <mleszczynski.dev@gmail.com>
2026-02-16 14:16:40 +0100

    Add trace log in TcpSocket

--- NetworkLib/src/TcpSocket.cpp
+++ NetworkLib/src/TcpSocket.cpp
@@ -1,13 +1,14 @@
 ﻿#include "TcpSocket.h"
 
+#include <spdlog/spdlog.h>
+
 TcpSocket::TcpSocket(asio::ip::tcp::socket&& socket)
 	: socket_(std::move(socket))
 {
-
+	SPDLOG_TRACE("");
 }
 
 TcpSocket::~TcpSocket()
 {
-
+	SPDLOG_TRACE("");
 }
-
