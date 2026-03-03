390a9d0552675b96ca3875bd8f8ce8d198e57a80
Michal <mleszczynski.dev@gmail.com>
2026-03-03 12:00:01 +0100

    Restructure ui project

--- EvolutionaryComputationUI/src/Application.cpp
+++ EvolutionaryComputationUI/src/Application.cpp
@@ -4,15 +4,13 @@
 #include <LoggerInitializer.h>
 #include <TcpClient.h>
 
-#include <asio/executor_work_guard.hpp>
-#include <asio/io_context.hpp>
-#include <QApplication>
-#include <spdlog/spdlog.h>
-
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <asio/executor_work_guard.hpp>
+#include <asio/io_context.hpp>
+#include <spdlog/spdlog.h>
 
-#include "../autogen/environment.h"
+#include "ui/App/autogen/environment.h"
 
 int Application::exec(int argc, char* argv[])
 {
@@ -22,38 +20,19 @@ int Application::exec(int argc, char* argv[])
 
 	SPDLOG_INFO("\n{}", settings.dump(4));
 
-#if 0
-	asio::io_context context;
+    SPDLOG_INFO(L"Current path: {}", std::filesystem::current_path().native());
 
-	std::uint16_t port = 2026;
-	if (port < 1024)
-	{
-		SPDLOG_ERROR("Tcp port number must be in range 1024 - 65535");
-		return 0;
-	}
-
-	auto client = TcpClient::create(context);
-	client->connectTo("localhost", port);
-	//server->setConnectionCallback([](AbstractSocket&& socket) { SPDLOG_INFO("New connection"); });
-	//if (!server->listen(port))
-	//{
-	//	SPDLOG_ERROR("TcpServer error!");
-	//	return 1;
-	//}
-
-	context.run();
-#endif
-
-    SPDLOG_INFO(L"{}", std::filesystem::current_path().native());
-
-	set_qt_environment();
+    set_qt_environment();
 	QApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
-	const QUrl url(mainQmlFile);
+
     for(const auto& path : engine.importPathList())
         SPDLOG_INFO("Import path: {}", path.toStdString());
+
+    const QUrl url(mainQmlFile);
     SPDLOG_INFO("Qml url: {}", mainQmlFile);
+
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
 		[url](QObject* obj, const QUrl& objUrl) {
 			if (!obj && url == objUrl)
@@ -72,3 +51,26 @@ int Application::exec(int argc, char* argv[])
 
 	return app.exec();
 }
+
+
+#if 0
+    asio::io_context context;
+
+    std::uint16_t port = 2026;
+    if (port < 1024)
+    {
+        SPDLOG_ERROR("Tcp port number must be in range 1024 - 65535");
+        return 0;
+    }
+
+    auto client = TcpClient::create(context);
+    client->connectTo("localhost", port);
+    //server->setConnectionCallback([](AbstractSocket&& socket) { SPDLOG_INFO("New connection"); });
+    //if (!server->listen(port))
+    //{
+    //	SPDLOG_ERROR("TcpServer error!");
+    //	return 1;
+    //}
+
+    context.run();
+#endif
